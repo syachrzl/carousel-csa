350dd5fee4913d85ba41a86e70f9702dcc0f9614
syahrizal <syachrzl@gmail.com>
2025-05-21 15:30:05 +0700

    Adding Carousel

--- app/Views/carousel/dashboard.php
+++ app/Views/carousel/dashboard.php
@@ -14,11 +14,41 @@
 
     <section class="content">
         <div class="container-fluid">
-
+            <div class="card">
+                <div class="card-body p-0">
+                    <iframe id="carouselFrame" src="" style="width: 100%; height: 90vh; border: none;"></iframe>
+                </div>
+            </div>
         </div>
     </section>
+
 </div>
 <?php $this->endSection(); ?>
 
 <?php $this->section('script'); ?>
+<script>
+    let idleTime = 0;
+    let isSecondPageShown = false;
+    const iframe = document.getElementById('carouselFrame');
+
+    // Reset idle timer on user activity
+    function resetIdleTimer() {
+        idleTime = 0;
+    }
+
+    // Cek idle setiap 1 detik
+    setInterval(() => {
+        idleTime++;
+        if (idleTime >= 60 && !isSecondPageShown) {
+            iframe.src = "";
+            isSecondPageShown = true;
+        }
+    }, 1000);
+
+    // Aktifitas user
+    window.addEventListener('mousemove', resetIdleTimer);
+    window.addEventListener('keypress', resetIdleTimer);
+    window.addEventListener('touchstart', resetIdleTimer);
+</script>
+
 <?php $this->endSection(); ?>
