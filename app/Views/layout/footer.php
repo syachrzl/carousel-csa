fc9c424f0a006f71d7dea57ab78cd8e10ffac195
syahrizal <syachrzl@gmail.com>
2025-05-21 16:12:03 +0700

    Change title

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -45,8 +45,6 @@
   <script src="<?= base_url('adminlte') ?>/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
   <!-- AdminLTE App -->
   <script src="<?= base_url('adminlte') ?>/dist/js/adminlte.js?v=3.2.0"></script>
-  <!-- AdminLTE for demo purposes -->
-  <script src="<?= base_url('adminlte') ?>/dist/js/demo.js"></script>
   <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
   <script src="<?= base_url('adminlte') ?>/dist/js/pages/dashboard.js"></script>
   <script defer src="https://static.cloudflareinsights.com/beacon.min.js/vcd15cbe7772f49c399c6a5babf22c1241717689176015" integrity="sha512-ZpsOmlRQV6y907TI0dKBHq9Md29nnaEIPlkf84rnaERnq6zvWvPUqr2ft8M1aS28oN72PdrCzSjY4U6VaAw1EQ==" data-cf-beacon='{"rayId":"943259844e8d4709","serverTiming":{"name":{"cfExtPri":true,"cfL4":true,"cfSpeedBrain":true,"cfCacheStatus":true}},"version":"2025.4.0-1-g37f21b1","token":"2437d112162f4ec4b63c3ca0eb38fb20"}' crossorigin="anonymous"></script>
