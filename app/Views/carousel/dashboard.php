8d1c1ff79216d901df32717d6cde13f09560fcef
syahrizal <syachrzl@gmail.com>
2025-05-21 15:22:53 +0700

    Slicing

---
+++ app/Views/carousel/dashboard.php
@@ -0,0 +1,24 @@
+<?php $this->extend('layout/wrapper'); ?>
+<!-- Dinamis CSS -->
+<?php $this->section('style'); ?>
+<?php $this->endSection(); ?>
+
+<!-- Dinamis Content -->
+<?php $this->section('content'); ?>
+<div class="content-wrapper">
+    <div class="content-header">
+        <div class="container-fluid">
+
+        </div>
+    </div>
+
+    <section class="content">
+        <div class="container-fluid">
+
+        </div>
+    </section>
+</div>
+<?php $this->endSection(); ?>
+
+<?php $this->section('script'); ?>
+<?php $this->endSection(); ?>
---
+++ app/Views/layout/navbar.php
@@ -0,0 +1,23 @@
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+
+        <!-- Preloader -->
+        <div class="preloader flex-column justify-content-center align-items-center">
+        </div>
+
+        <!-- Navbar -->
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <!-- Left navbar links -->
+            <ul class="navbar-nav">
+            </ul>
+
+            <!-- Right navbar links -->
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+                        <i class="fas fa-expand-arrows-alt"></i>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <!-- /.navbar -->
