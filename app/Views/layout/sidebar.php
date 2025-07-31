89ffc45a82f97e066ab83450837aa73c630eb5ad
syahrizal <syachrzl@gmail.com>
2025-07-31 08:41:08 +0700

    Modify sidebar

--- app/Views/layout/sidebar.php
+++ app/Views/layout/sidebar.php
@@ -1,110 +1,108 @@
-<!-- Main Sidebar Container -->
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+        <!-- Main Sidebar Container -->
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">
 
-    <!-- Sidebar -->
-    <div class="sidebar">
-
-
-        <!-- SidebarSearch Form -->
-        <div class="form-inline">
-            <!-- Sidebar Menu -->
-            <nav class="mt-2">
-                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                    <li class="nav-item menu-open">
-                        <a href="#" class="nav-link active">
-                            <i class="nav-icon fas fa-tachometer-alt"></i>
-                            <p>
-                                Dashboard
-                                <i class="right fas fa-angle-left"></i>
-                            </p>
-                        </a>
-                        <ul class="nav nav-treeview">
-                            <li class="nav-item">
-                                <a href="#" class="nav-link report-btn" data-src="https://app.powerbi.com/view?r=eyJrIjoiMDYyYWVkMDYtMDBhYi00MWE3LWFhNmQtNTM3Yjc4NDI0NmI4IiwidCI6IjVjODQ2NDU0LTI4MjMtNGNhZi04ZThhLWNmZjY5NjUzYjMyNiIsImMiOjEwfQ%3D%3D">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>Commercial</p>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="#" class="nav-link report-btn" data-src="https://app.powerbi.com/view?r=eyJrIjoiMmQyZDFmMWQtZjVmOC00NDUyLThjNzctOWNkOGQ4MWM5MjhiIiwidCI6IjVjODQ2NDU0LTI4MjMtNGNhZi04ZThhLWNmZjY5NjUzYjMyNiIsImMiOjEwfQ%3D%3D">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>Corporate Strategy Map</p>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="#" class="nav-link report-btn" data-src="https://app.powerbi.com/view?r=eyJrIjoiNTUyM2I4NzYtMTcyOS00NjUyLWE4YzYtMWVjYjc4M2IwZWQ4IiwidCI6IjVjODQ2NDU0LTI4MjMtNGNhZi04ZThhLWNmZjY5NjUzYjMyNiIsImMiOjEwfQ%3D%3D">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>Finance - Opex</p>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="#" class="nav-link report-btn" data-src="https://app.powerbi.com/view?r=eyJrIjoiMmM1MDdmMTAtYzJjNy00MGIzLTk0NzQtZjM4MzgxZDliM2YwIiwidCI6IjVjODQ2NDU0LTI4MjMtNGNhZi04ZThhLWNmZjY5NjUzYjMyNiIsImMiOjEwfQ%3D%3D">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>Human Resources</p>
-                                </a>
-                            </li>
+            <!-- Sidebar -->
+            <div class="sidebar">
 
 
-                            <li class="nav-item">
-                                <a href="#" class="nav-link report-btn" data-src="https://app.powerbi.com/view?r=eyJrIjoiYWVmYzVmOTEtZjk1Zi00NDBmLTllNzMtMzdmYWExNGJiYTcxIiwidCI6IjVjODQ2NDU0LTI4MjMtNGNhZi04ZThhLWNmZjY5NjUzYjMyNiIsImMiOjEwfQ%3D%3D">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>OM Performance</p>
+                <!-- SidebarSearch Form -->
+                <div class="form-inline">
+                    <!-- Sidebar Menu -->
+                    <nav class="mt-2">
+                        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                            <li class="nav-item menu-open">
+                                <a href="#" class="nav-link active">
+                                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                                    <p>
+                                        Dashboard
+                                        <i class="right fas fa-angle-left"></i>
+                                    </p>
                                 </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="#" class="nav-link report-btn" data-src="https://app.powerbi.com/view?r=eyJrIjoiNGMyODQwYjgtNzg0Zi00YWMwLTk3NDItMzE2NTE2NTIwZjIyIiwidCI6IjVjODQ2NDU0LTI4MjMtNGNhZi04ZThhLWNmZjY5NjUzYjMyNiIsImMiOjEwfQ%3D%3D">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>OM TT Fiber</p>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="#" class="nav-link report-btn" data-src="https://app.powerbi.com/view?r=eyJrIjoiYTk3Mjg1NGItYjNkNi00Y2MxLTg0N2ItM2E0N2QxZWQxZGVkIiwidCI6IjVjODQ2NDU0LTI4MjMtNGNhZi04ZThhLWNmZjY5NjUzYjMyNiIsImMiOjEwfQ%3D%3D">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>OM TT Tower</p>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="#" class="nav-link report-btn" data-src="https://app.powerbi.com/view?r=eyJrIjoiN2VjZGU4MjgtZjQ1MC00ZjRlLWIwNDItYTc0NTBmYjExYzljIiwidCI6IjVjODQ2NDU0LTI4MjMtNGNhZi04ZThhLWNmZjY5NjUzYjMyNiIsImMiOjEwfQ%3D%3D">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p> PMO Service CME</p>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="#" class="nav-link report-btn" data-src="https://app.powerbi.com/view?r=eyJrIjoiZDAxYThmMmItNGQxYy00MDcxLWFjN2EtZTQyMWU3ZjA1NmQ3IiwidCI6IjVjODQ2NDU0LTI4MjMtNGNhZi04ZThhLWNmZjY5NjUzYjMyNiIsImMiOjEwfQ%3D%3D">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>PMO Service FO</p>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="#" class="nav-link report-btn" data-src="https://app.powerbi.com/view?r=eyJrIjoiMjFjM2ZiYzAtZWNiZC00NmI3LWI0ZjEtZmVkMjNiMWMyNmQzIiwidCI6IjVjODQ2NDU0LTI4MjMtNGNhZi04ZThhLWNmZjY5NjUzYjMyNiIsImMiOjEwfQ%3D%3D">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>Project CME</p>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="#" class="nav-link report-btn" data-src="https://app.powerbi.com/view?r=eyJrIjoiZmI0ZjA0MDAtOTIzMS00NGU5LWJiZjAtMTEwZWQ0NDUxYzc0IiwidCI6IjVjODQ2NDU0LTI4MjMtNGNhZi04ZThhLWNmZjY5NjUzYjMyNiIsImMiOjEwfQ%3D%3D">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>Project Enterprise</p>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="#" class="nav-link report-btn" data-src="https://app.powerbi.com/view?r=eyJrIjoiZjQ4ZTQ1YmMtOTJhNS00MmYwLThlNjAtOTQ0OTEwZTc5YmUzIiwidCI6IjVjODQ2NDU0LTI4MjMtNGNhZi04ZThhLWNmZjY5NjUzYjMyNiIsImMiOjEwfQ%3D%3D">
-                                    <i class=" far fa-circle nav-icon"></i>
-                                    <p>Project FO</p>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="#" class="nav-link report-btn" data-src="https://app.powerbi.com/view?r=eyJrIjoiZjg5MzlkMGMtNThlMS00N2IwLTliNjMtNzgwYzA3ODc2Njk3IiwidCI6IjVjODQ2NDU0LTI4MjMtNGNhZi04ZThhLWNmZjY5NjUzYjMyNiIsImMiOjEwfQ%3D%3D">
-                                    <i class=" far fa-circle nav-icon"></i>
-                                    <p>Project FTTH</p>
-                                </a>
+                                <ul class="nav nav-treeview">
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link report-btn" data-src="https://app.powerbi.com/view?r=eyJrIjoiMDYyYWVkMDYtMDBhYi00MWE3LWFhNmQtNTM3Yjc4NDI0NmI4IiwidCI6IjVjODQ2NDU0LTI4MjMtNGNhZi04ZThhLWNmZjY5NjUzYjMyNiIsImMiOjEwfQ%3D%3D">
+                                            <i class="far fa-circle nav-icon"></i>
+                                            <p>Commercial</p>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link report-btn" data-src="https://app.powerbi.com/view?r=eyJrIjoiMmQyZDFmMWQtZjVmOC00NDUyLThjNzctOWNkOGQ4MWM5MjhiIiwidCI6IjVjODQ2NDU0LTI4MjMtNGNhZi04ZThhLWNmZjY5NjUzYjMyNiIsImMiOjEwfQ%3D%3D">
+                                            <i class="far fa-circle nav-icon"></i>
+                                            <p>Corporate Strategy Map</p>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link report-btn" data-src="https://app.powerbi.com/view?r=eyJrIjoiNTUyM2I4NzYtMTcyOS00NjUyLWE4YzYtMWVjYjc4M2IwZWQ4IiwidCI6IjVjODQ2NDU0LTI4MjMtNGNhZi04ZThhLWNmZjY5NjUzYjMyNiIsImMiOjEwfQ%3D%3D">
+                                            <i class=" far fa-circle nav-icon"></i>
+                                            <p>Finance - Opex</p>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link report-btn" data-src="https://app.powerbi.com/view?r=eyJrIjoiMmM1MDdmMTAtYzJjNy00MGIzLTk0NzQtZjM4MzgxZDliM2YwIiwidCI6IjVjODQ2NDU0LTI4MjMtNGNhZi04ZThhLWNmZjY5NjUzYjMyNiIsImMiOjEwfQ%3D%3D">
+                                            <i class="far fa-circle nav-icon"></i>
+                                            <p>Human Resources</p>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link report-btn" data-src="https://app.powerbi.com/view?r=eyJrIjoiYWVmYzVmOTEtZjk1Zi00NDBmLTllNzMtMzdmYWExNGJiYTcxIiwidCI6IjVjODQ2NDU0LTI4MjMtNGNhZi04ZThhLWNmZjY5NjUzYjMyNiIsImMiOjEwfQ%3D%3D">
+                                            <i class=" far fa-circle nav-icon"></i>
+                                            <p>OM Performance</p>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link report-btn" data-src="https://app.powerbi.com/view?r=eyJrIjoiNGMyODQwYjgtNzg0Zi00YWMwLTk3NDItMzE2NTE2NTIwZjIyIiwidCI6IjVjODQ2NDU0LTI4MjMtNGNhZi04ZThhLWNmZjY5NjUzYjMyNiIsImMiOjEwfQ%3D%3D">
+                                            <i class=" far fa-circle nav-icon"></i>
+                                            <p>OM TT Fiber</p>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link report-btn" data-src="https://app.powerbi.com/view?r=eyJrIjoiYTk3Mjg1NGItYjNkNi00Y2MxLTg0N2ItM2E0N2QxZWQxZGVkIiwidCI6IjVjODQ2NDU0LTI4MjMtNGNhZi04ZThhLWNmZjY5NjUzYjMyNiIsImMiOjEwfQ%3D%3D">
+                                            <i class=" far fa-circle nav-icon"></i>
+                                            <p>OM TT Tower</p>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link report-btn" data-src="https://app.powerbi.com/view?r=eyJrIjoiN2VjZGU4MjgtZjQ1MC00ZjRlLWIwNDItYTc0NTBmYjExYzljIiwidCI6IjVjODQ2NDU0LTI4MjMtNGNhZi04ZThhLWNmZjY5NjUzYjMyNiIsImMiOjEwfQ%3D%3D">
+                                            <i class="far fa-circle nav-icon"></i>
+                                            <p> PMO Service CME</p>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link report-btn" data-src="https://app.powerbi.com/view?r=eyJrIjoiZDAxYThmMmItNGQxYy00MDcxLWFjN2EtZTQyMWU3ZjA1NmQ3IiwidCI6IjVjODQ2NDU0LTI4MjMtNGNhZi04ZThhLWNmZjY5NjUzYjMyNiIsImMiOjEwfQ%3D%3D">
+                                            <i class="far fa-circle nav-icon"></i>
+                                            <p>PMO Service FO</p>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link report-btn" data-src="https://app.powerbi.com/view?r=eyJrIjoiMjFjM2ZiYzAtZWNiZC00NmI3LWI0ZjEtZmVkMjNiMWMyNmQzIiwidCI6IjVjODQ2NDU0LTI4MjMtNGNhZi04ZThhLWNmZjY5NjUzYjMyNiIsImMiOjEwfQ%3D%3D">
+                                            <i class="far fa-circle nav-icon"></i>
+                                            <p>Project CME</p>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link report-btn" data-src="https://app.powerbi.com/view?r=eyJrIjoiMDk2MmMwN2MtMzZkYy00NTY4LWE4YjUtYjcyMGQ0NDVjY2IyIiwidCI6IjVjODQ2NDU0LTI4MjMtNGNhZi04ZThhLWNmZjY5NjUzYjMyNiIsImMiOjEwfQ%3D%3D">
+                                            <i class=" far fa-circle nav-icon"></i>
+                                            <p>Project FO</p>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link report-btn" data-src="https://app.powerbi.com/view?r=eyJrIjoiZmI0ZjA0MDAtOTIzMS00NGU5LWJiZjAtMTEwZWQ0NDUxYzc0IiwidCI6IjVjODQ2NDU0LTI4MjMtNGNhZi04ZThhLWNmZjY5NjUzYjMyNiIsImMiOjEwfQ%3D%3D">
+                                            <i class=" far fa-circle nav-icon"></i>
+                                            <p>Project Enterprise</p>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link report-btn" data-src="https://app.powerbi.com/view?r=eyJrIjoiZjg5MzlkMGMtNThlMS00N2IwLTliNjMtNzgwYzA3ODc2Njk3IiwidCI6IjVjODQ2NDU0LTI4MjMtNGNhZi04ZThhLWNmZjY5NjUzYjMyNiIsImMiOjEwfQ%3D%3D">
+                                            <i class=" far fa-circle nav-icon"></i>
+                                            <p>Project FTTH</p>
+                                        </a>
+                                    </li>
+                                </ul>
                             </li>
                         </ul>
-                    </li>
-                </ul>
 
-            </nav>
-            <!-- /.sidebar-menu -->
-        </div>
-        <!-- /.sidebar -->
-</aside>
+                    </nav>
+                    <!-- /.sidebar-menu -->
+                </div>
+                <!-- /.sidebar -->
+        </aside>
